e3c22eeab3cdd432aa406828474d67448876fad4
regarry <regarry@ncsu.edu>
2023-12-18 15:26:14 -0500

    formatted mask2vertices and added to generate cube dataset

--- Pytorch-UNet/GenerateCubeDataset.py
+++ Pytorch-UNet/GenerateCubeDataset.py
@@ -12,13 +12,14 @@ from PIL import Image
 import SliceVolume as sv
 from tqdm import tqdm
 import time
+import Mask2Vertices as m2v
 
 volume_size = 512
-cube_sizes = list(range(volume_size//2, volume_size, (volume_size-volume_size//2)//5))
+cube_sizes = list(range(volume_size//2, volume_size, (volume_size-volume_size//2)//4))
 image_format = '.png'
-input_path = './brain_1/masks/'
-output_dir =  './cube/'
-angles = list(range(10, 360, 90))
+#input_path = './brain_1/masks/'
+output_dir =  './cube_2/'
+angles = list(range(10, 90, 20))
 dimensions = [[0,1],[0,2],[1,2]]
 
 if not os.path.exists(output_dir):
@@ -30,10 +31,11 @@ if not os.path.exists(output_dir):
 for cube_size in tqdm(cube_sizes):
     for angle in angles:
         for dimension in dimensions:
-            output_path = output_dir + f'masks_{cube_size}_{angle}_{dimension[0]}{dimension[1]}/'
+            output_path = output_dir + f'masks_{cube_size*100//volume_size:02}_{angle}_{dimension[0]}{dimension[1]}/'
             if not os.path.exists(output_path):
                 os.makedirs(output_path)
             volume = sv.create_cube(volume_size, cube_size)
             sv.oblique_slice(volume, angle, dimension, image_format, output_path)
             output = sv.stack_images_to_volume(output_path, image_format)
             savemat(output_path + f'cube_{angle}_{dimension[0]}{dimension[1]}.mat', {'output': output, 'input': volume})
+            m2v.mask2vertices(output_path, image_format, 2)
--- Pytorch-UNet/Mask2Vertices.py
+++ Pytorch-UNet/Mask2Vertices.py
@@ -1,52 +1,51 @@
 import cv2
 import os
-import scipy.ndimage as ndi
 import numpy as np
-import matplotlib.pyplot as plt
+from scipy.io import savemat
 
-minimum_coordinates = 100
-dir_path = './brain_1/yz_45_masks/'
-image_format = '.png'
 
-if not os.path.exists(f'{dir_path}/markers/'):
-    os.makedirs(f'{dir_path}/markers/')
-if not os.path.exists(f'{dir_path}/contours/'):
-    os.makedirs(f'{dir_path}/contours/')
+def mask2vertices(mask_path, image_format, minimum_coordinates):
+    if not os.path.exists(f'{mask_path}/markers/'):
+        os.makedirs(f'{mask_path}/markers/')
+    if not os.path.exists(f'{mask_path}/contours/'):
+        os.makedirs(f'{mask_path}/contours/')
     
-# Get a list of all image files in the directory with the specified format
-image_files = []
-dfs = []
-for file in os.listdir(dir_path):
-    if file.endswith(image_format):
-        image_files.append(file)
-image_file_paths = [os.path.join(dir_path, file) for file in image_files]
-for file_path, file_name in zip(image_file_paths, image_files):
-    mask = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
-    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Get a list of all image files in the directory with the specified format
+    image_files = []
+    dfs = []
+    for file in os.listdir(mask_path):
+        if file.endswith(image_format):
+            image_files.append(file)
+    image_file_paths = [os.path.join(mask_path, file) for file in image_files]
+    for file_path, file_name in zip(image_file_paths, image_files):
+        mask = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # display the contours and coordinates in png files
-    contour = np.zeros(mask.shape, dtype='uint8')
-    marker = np.zeros(mask.shape, dtype='uint8')
-    for i in range(len(contours)):
-        coordinates = np.array(contours[i]).reshape((-1,2))
-        if coordinates.shape[0] > minimum_coordinates:
-            cv2.drawContours(contour, contours=contours, contourIdx=i, color=(255, 255, 255), thickness=1)
-            for coordinate in coordinates:
-                cv2.drawMarker(marker,coordinate, color=(255, 255, 255), markerType=cv2.MARKER_CROSS, markerSize=1, thickness=1)
-        
-    cv2.imwrite(f'{dir_path}/markers/{file_name[:len(file_name)-len(image_format)]}_markers.png', marker)
-    cv2.imwrite(f'{dir_path}/contours/{file_name[:len(file_name)-len(image_format)]}_contours.png', contour)
+        # display the contours and coordinates in png files
+        contour = np.zeros(mask.shape, dtype='uint8')
+        marker = np.zeros(mask.shape, dtype='uint8')
+        for i in range(len(contours)):
+            coordinates = np.array(contours[i]).reshape((-1,2))
+            if coordinates.shape[0] > minimum_coordinates:
+                cv2.drawContours(contour, contours=contours, contourIdx=i, color=(255, 255, 255), thickness=1)
+                for coordinate in coordinates:
+                    cv2.drawMarker(marker,coordinate, color=(255, 255, 255), markerType=cv2.MARKER_CROSS, markerSize=1, thickness=1)
+            
+        cv2.imwrite(f'{mask_path}/markers/{file_name[:len(file_name)-len(image_format)]}_markers.png', marker)
+        cv2.imwrite(f'{mask_path}/contours/{file_name[:len(file_name)-len(image_format)]}_contours.png', contour)
 
-    # make the combined coordinate array
-    total_coordinates = np.full((2, 2), np.nan)
-    for i in range(len(contours)):
-        coordinates = np.array(contours[i]).reshape((-1,2))
-        if coordinates.shape[0] > minimum_coordinates:
-            print(coordinates.shape)
-            total_coordinates = np.append(total_coordinates, coordinates, axis=0) 
-    total_coordinates = total_coordinates[2:,:] # remove the first two rows of nan
-    print(total_coordinates.shape)
-    dfs.append(total_coordinates)
+        # make the combined coordinate array
+        total_coordinates = np.full((2, 2), np.nan)
+        for i in range(len(contours)):
+            coordinates = np.array(contours[i]).reshape((-1,2))
+            if coordinates.shape[0] > minimum_coordinates:
+                #print(coordinates.shape)
+                total_coordinates = np.append(total_coordinates, coordinates, axis=0) 
+        total_coordinates = total_coordinates[2:,:] # remove the first two rows of nan
+        #print(total_coordinates.shape)
+        dfs.append(total_coordinates)
 
-arr = np.array(dfs,dtype=object)
-np.save(dir_path + 'arr.npy', arr)
+    arr = np.array(dfs,dtype=object)
+    np.save(f'{mask_path}/contours/' + 'vertices.npy', arr)
+    savemat(f'{mask_path}/contours/' + 'vertices.mat', {'vertices': arr})
+    
